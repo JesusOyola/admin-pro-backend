77cc16576221634a927ecde2f5ff4c59debfc5bd
JesusOyola <jesus.oyola@interatica.com>
2023-08-07 13:26:11 -0300

    login con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const Usuario = require("../models/usuario")
 const bcrypt = require("bcryptjs")
 const { generarJWT } = require("../helpers/jwt")
+const { googleVerify } = require("../helpers/google-verify")
 
 const login = async (req, res) => {
   const { email, password } = req.body
@@ -48,6 +49,52 @@ const login = async (req, res) => {
   }
 }
 
+const googleSignIn = async (req, res) => {
+  try {
+    const { email, name, picture } = await googleVerify(
+      req.body.token
+    )
+
+    const usuarioDB = await Usuario.findOne({ email })
+    let usuario
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email: email,
+        password: "@@",
+        img: picture,
+        google: true,
+      })
+    } else {
+      usuario = usuarioDB
+      usuario.google = true
+    }
+
+    // Guardar Usuario
+    await usuario.save()
+
+    //Generar el token
+
+    const token = await generarJWT(usuario.id)
+
+    res.json({
+      ok: true,
+      email,
+      name,
+      picture,
+      token,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      ok: false,
+      msg: "Token de google no es correcto",
+    })
+  }
+}
+
 module.exports = {
   login,
+  googleSignIn,
 }
